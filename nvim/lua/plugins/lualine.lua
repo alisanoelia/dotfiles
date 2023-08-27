0a71227dc35f070ea8d74a98d9ed2e9c0a2c47f5
alyssa <alyssa>
2023-08-27 02:51:34 +0000

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -23,10 +23,10 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { { 'filename', path = 3 } },
-        lualine_c = { 'diagnostics' },
-        lualine_x = {},
-        lualine_y = { '' },
+        lualine_b = { 'branch', { 'filename', path = 3 } },
+        lualine_c = { 'diagnostics', },
+        lualine_x = { 'diff', 'fileformat' },
+        lualine_y = { },
         lualine_z = { 'filetype' }
       },
       inactive_sections = {

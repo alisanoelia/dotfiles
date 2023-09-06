0be2d03e8759c931cd28b4a29059668e6a0d9e16
alyssa <alyssa>
2023-09-06 23:26:54 +0000

    update config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
-  "sainnhe/everforest",
-
-  config = function ()
-    vim.cmd('colorscheme everforest')
-  end
+  "mbrea-c/wal-colors.nvim",
+  config = function()
+    vim.cmd([[colorscheme mbc]]) -- activate the colorscheme
+  end,
+  priority = 1000, -- recommended to ensure the colorscheme
+                   -- is loaded before other plugins
 }

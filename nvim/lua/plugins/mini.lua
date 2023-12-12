fe5596171c4ab9c73981b651393b9818eea4f3c2
alyssa <alyssa>
2023-12-12 20:04:58 +0000

    add: indent

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,9 +1,13 @@
 return {
   'echasnovski/mini.nvim',
 	config = function()
+    require('mini.ai').setup()
     require('mini.comment').setup()
-    require('mini.cursorword').setup()
-    require('mini.pairs').setup()
+    -- require('mini.cursorword').setup()
+		require('mini.indentscope').setup({
+			symbol = '▏'
+		})
+		require('mini.pairs').setup()
     require('mini.surround').setup()
   end
 }

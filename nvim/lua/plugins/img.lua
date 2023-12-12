df320b5c571679e1ab9f8c34781a1c385a6b1306
alyssa <alyssa>
2023-12-12 20:04:34 +0000

    update: clean plugins

--- nvim/lua/plugins/img.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "HakonHarnes/img-clip.nvim",
-  cmd = "PasteImage",
-  keys = {
-    -- suggested keymap
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
-  },
-}
--- nvim/lua/plugins/screenshot.lua
+++
@@ -1,4 +0,0 @@
-return {
-	"segeljakt/vim-silicon"
-}
-

90ab9f1283da7338bdd9353959717620a4b8b9c9
alyssa <alyssa>
2024-04-11 00:39:13 +0000

    fix: cursor gui

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -37,7 +37,7 @@ opt.showmode = false
 
 -- opt.guicursor = ""
 vim.o.guicursor = table.concat({
-  "n-v-c:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
   "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
   "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
 }, ",")

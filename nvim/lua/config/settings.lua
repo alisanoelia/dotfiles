0c2dae4bb02d2cc8064d9f1428b09287109446bd
alyssa <alyssa>
2024-04-11 00:35:29 +0000

    update: fix and add new changes

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -34,3 +34,10 @@ opt.signcolumn = 'yes'
 opt.path:append { '**' }
 
 opt.showmode = false
+
+-- opt.guicursor = ""
+vim.o.guicursor = table.concat({
+  "n-v-c:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")

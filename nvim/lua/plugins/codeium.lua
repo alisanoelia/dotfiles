0219b01933f7d95fe5b26524b76405f9ec77b4bf
asha <asha>
2024-12-15 03:55:40 -0500

    [fix]: error codeium.nvim

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,24 +1,37 @@
--- return {
--- 	"Exafunction/codeium.nvim",
--- 	lazy = false,
--- 	config = function()
--- 		require('codeium').setup({
--- 			enable_chat = true,
--- 		})
--- 	end
--- }
+-- plugin for autocomplete 
+
+return {
+	"Exafunction/codeium.nvim",
+	lazy = false,
+	config = function()
+		require('codeium').setup({
+			enable_chat = true,
+			enable_cmp_source = false,
+			virtual_text = {
+				enabled = true,
+				map_keys = true,
+				key_bindings = {
+					accept = "<C-k>",
+					next = "<C-]>",
+					prev = "<C-[>",
+				}
+			},
+		})
+	end
+}
+
 
 -- error plenary plugin
 
-return {
-	'Exafunction/codeium.vim',
-	config = function()
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set('i', '<C-k>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-		vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-			{ expr = true, silent = true })
-		vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-			{ expr = true, silent = true })
-		vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-	end
-}
+-- return {
+-- 	'Exafunction/codeium.vim',
+-- 	config = function()
+-- 		-- Change '<C-g>' here to any keycode you like.
+-- 		vim.keymap.set('i', '<C-k>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+-- 		vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+-- 			{ expr = true, silent = true })
+-- 		vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+-- 			{ expr = true, silent = true })
+-- 		vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+-- 	end
+-- }

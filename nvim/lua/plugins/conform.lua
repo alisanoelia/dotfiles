cea6692a8485a642cd71a5e57d1c791d569ab38e
asha <asha>
2024-12-31 10:24:57 -0500

    [fix]: error formatter code

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,30 +1,31 @@
 return {
-	'stevearc/conform.nvim',
-	lazy = true,
-	enabled = false,
-	event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+	"stevearc/conform.nvim",
+	enabled = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 	config = function()
-		require('conform').setup {
+		require("conform").setup({
 			formatters_by_ft = {
-				lua = { 'stylua' },
-				javascript = { 'eslint_d' },
-				typescript = { 'eslint_d' },
-				javascriptreact = { 'eslint_d' },
-				typescriptreact = { 'eslint_d' },
-				css = { 'prettierd' },
-				html = { 'prettierd' },
-				json = { 'prettierd' },
-				yaml = { 'prettierd' },
-				markdown = { 'prettierd' },
-				graphql = { 'prettierd' },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				shell = { "shfmt" },
+				bash = { "shfmt" },
 			},
 
 			format_on_save = {
-				pattern = '.lua,*.graphql,*.css,*.html,*.json,*.yaml,*.md,*.gql,*.rs',
-				timeout_ms = 500,
+				pattern = ".lua,*.graphql,*.css,*.html,*.json,*.yaml,*.md,*.gql,*.rs, *.js, *.ts, *.jsx, *.tsx, *.sh",
+				timeout_ms = 10000,
 				async = false,
 				lsp_fallback = true,
 			},
-		}
+		})
 	end,
 }

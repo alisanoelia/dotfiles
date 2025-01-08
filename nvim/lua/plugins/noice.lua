1e066fcf1b2cffe3092fb0b65ce06aa198efe2e1
asha <asha>
2025-01-08 17:50:55 -0500

    [add]: noice.nvim

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,59 @@
+-- noice plugin
+
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+
+				command_palette = false,
+				long_message_to_split = true,
+				inc_rename = true,
+				lsp_doc_border = true,
+			},
+			routes = {
+				{
+					filter = {
+						event = "notify",
+						find = "No information available",
+					},
+					opts = { skip = true },
+				},
+			},
+			cmdline = {
+				format = {
+					search_down = { kind = "search", pattern = "^/", icon = ">", lang = "regex" },
+				},
+			},
+		})
+		require("notify").setup({
+			fps = 75,
+			stages = "fade",
+			render = "minimal",
+			background_colour = "#1e222a",
+			timeout = 1000,
+			top_down = true,
+			icons = {
+				ERROR = "",
+				WARN = "",
+				INFO = "",
+				DEBUG = "",
+				TRACE = "✎",
+			},
+		})
+	end,
+}
